ab1fe0758eb1298b9b723d93f4b4a939547bbd78
pegasie <dinoa2003@hotmail.com>
2018-09-17 13:01:15 -0400

    Refactored 'clickLogout' method

--- src/main/java/com/pegasie/pages/cia/HomePage.java
+++ src/main/java/com/pegasie/pages/cia/HomePage.java
@@ -2,20 +2,19 @@ package com.pegasie.pages.cia;
 
 import com.pegasie.pages.TemplatePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class HomePage extends TemplatePage {
 
     @FindBy(how = How.XPATH, using = "//h1[@class='heading']")
         WebElement lblPageTitle;
-
-    @FindBy(how = How.XPATH, using = "//div[@class='flex-container']/header[@id='header']")
-    WebElement containerHeader;
-
     @FindBy(how = How.XPATH, using = "//a[@href='/ws/user/logout']")
     WebElement lnkLogout;
 
@@ -25,10 +24,19 @@ public class HomePage extends TemplatePage {
         return true;
     }
 
-    public void clickLogout() {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='flex-container']/header[@id='header']")));
-        wait.until(ExpectedConditions.elementToBeClickable(lnkLogout));
-        lnkLogout.click();
+    public boolean clickLogout() {
+        boolean result = false;
+        try {
+            waitPageReady();
+            lnkLogout.click();
+            result = true;
+        }
+        catch (NoSuchElementException ex) {
+            System.out.println("<<" +ex.getLocalizedMessage() + ">>");
+        }
+        finally {
+            return result;
+        }
+
     }
 }
