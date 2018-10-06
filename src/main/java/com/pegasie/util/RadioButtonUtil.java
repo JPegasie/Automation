659b38da3fd86f17a7283a0f319f3876e4264dcd
pegasie <dinoa2003@hotmail.com>
2018-10-06 18:57:05 -0400

    Added new class in util package named RadioButtonUtil.

---
+++ src/main/java/com/pegasie/util/RadioButtonUtil.java
@@ -0,0 +1,29 @@
+package com.pegasie.util;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class RadioButtonUtil {
+
+   public static boolean selectByValue(List<WebElement> toggleButtons, String text) {
+        boolean result = false;
+        if (text.length() != 0) {
+            for (WebElement toggle : toggleButtons) {
+                System.out.println(toggle.getAttribute("value") + " the actual view value");
+                if (toggle.getAttribute("value").equalsIgnoreCase(text)) {
+                    WebElement e = toggle.findElement(By.xpath("following-sibling::label"));
+                    e.click();
+                    result = true;
+                    break;
+                }
+            }
+        }
+        else {
+            result = true;
+        }
+        return result;
+    }
+}
