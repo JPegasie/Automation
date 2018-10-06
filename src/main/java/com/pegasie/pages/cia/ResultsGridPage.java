3a5428b4ff83beba178e65f4d72a2bfb33bfef97
pegasie <dinoa2003@hotmail.com>
2018-10-05 22:52:04 -0400

    Created new page ResultsGridPage from HomePage. HomePage removed.

---
+++ src/main/java/com/pegasie/pages/cia/ResultsGridPage.java
@@ -0,0 +1,66 @@
+package com.pegasie.pages.cia;
+
+import com.pegasie.pages.TemplatePage;
+import com.pegasie.util.HTMLTableUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class ResultsGridPage extends TemplatePage {
+
+    @FindBy(how = How.XPATH, using = "//h1[@class='heading']")
+        WebElement lblPageTitle;
+    @FindBy(how = How.XPATH, using = "//a[@href='/ws/user/logout']")
+    WebElement lnkLogout;
+    @FindBy(how = How.XPATH, using = "//span[@data-bind='text : affectedRequirements']")
+        WebElement lblAffectedFeaturesCnt;
+
+    public boolean verifyContentTitle (String title) {
+        System.out.println(lblPageTitle.getTagName());
+        //return lblPageTitle.getText().equalsIgnoreCase(title);
+        return true;
+    }
+
+    public boolean clickLogout() {
+        boolean result = false;
+        try {
+            waitPageReady();
+            lnkLogout.click();
+            result = true;
+        }
+        catch (NoSuchElementException ex) {
+            System.out.println("<<" +ex.getLocalizedMessage() + ">>");
+        }
+        finally {
+            return result;
+        }
+    }
+
+    public boolean verifyAffectedFeaturesCnt(String lblCount) {
+        boolean result = false;
+        try {
+            waitPageReady();
+            if (lblAffectedFeaturesCnt.getText().contains(lblCount)) {
+                result = true;
+            }
+        }
+        catch (NoSuchElementException ex) {
+            System.out.println("<<" +ex.getLocalizedMessage() + ">>");
+        }
+        finally {
+            return result;
+        }
+    }
+
+    public boolean verifyLogoutLink() {
+        boolean result = false;
+       //waitPageReady();
+        return lnkLogout.getText().equalsIgnoreCase("logout");
+    }
+}
