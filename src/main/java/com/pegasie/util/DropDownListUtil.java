762b9e370581cf60a3b6d5ea2dac69e1b2b57681
pegasie <dinoa2003@hotmail.com>
2018-10-06 17:04:00 -0400

    Modified method selectByVisibleText to return boolean result.

--- src/main/java/com/pegasie/util/DropDownListUtil.java
+++ src/main/java/com/pegasie/util/DropDownListUtil.java
@@ -7,14 +7,22 @@ import java.util.List;
 
 public class DropDownListUtil {
 
-    public static void selectByVisibleText(WebElement select, String text) {
-        List<WebElement> options = select.findElements(By.tagName("option"));
-        for (WebElement option : options) {
-            if (option.getText().equals(text)) {
-                option.click();
-                break;
+    public static boolean selectByVisibleText(WebElement select, String text) {
+        boolean result = false;
+        if (text.length() != 0) {
+            List<WebElement> options = select.findElements(By.tagName("option"));
+            for (WebElement option : options) {
+                if (option.getText().equals(text)) {
+                    option.click();
+                    result = true;
+                    break;
+                }
             }
         }
+        else {
+            result = true;
+        }
+        return result;
     }
 
     public static void selectByValue(WebElement select, String value) {
